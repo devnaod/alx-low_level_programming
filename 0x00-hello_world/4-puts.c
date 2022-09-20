22a2bdd4caa6b0bced9c52d5058d6460d26d35eb
NAOD ADNEW <devnaod@gmail.com>
2022-09-20 01:15:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/*
-* Function to print string using puts() function
+/**
+	* Function to print string using puts() function
+	* return 0
 */
 
-int main()
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
 	
