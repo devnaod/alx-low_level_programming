dbf403924b02c08a4556df793fb65e258e761ff4
NAOD ADNEW <devnaod@gmail.com>
2022-09-21 03:46:36 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * By : Naod Adnew
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d bytes \n", sizeof(char));
+	printf("Size of int is %d bytes \n", sizeof(int));
+	printf("size of long long int: %d bytes \n", sizeof(long long int));
+	printf("size of float: %d bytes \n", sizeof(float));
+return 0;
+}
